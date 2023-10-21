d0706e18a3847eb429347dd60b9a0d1d0e8035f4
AbhiramKrishnaM <abhiramkrishna.8921@gmail.com>
2023-10-21 23:50:47 +0530

    removed readme file

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,7 @@
 <template>
-  <h1 class="text-7xl">Hello world!</h1>
+  <h1
+    class="text-7xl w-screen h-screen bg-theme-main text-white-gradient-01 flex items-center justify-center"
+  >
+    Snake game
+  </h1>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "theme-main": "#011627",
+        "theme-main-gradient": "#011627D6",
+        "accent-color": "#607B96",
+        "accent-sub": "#4D5BCE",
+        "accent-url": "#E99287",
+        "accent-variable": "#43D9AD",
+        "accent-underline": "#FEA55F",
+
+        // text color
+        "white-gradient-01": "#E5E9F0",
+        "gray-gradient-01": "#607B96",
+      },
+    },
   },
   plugins: [],
 };
