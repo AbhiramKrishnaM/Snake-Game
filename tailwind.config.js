b3ecc1453cd57f8109e1b9aeb3d09bc9585b95a8
Abhiram Krishna M <abhiramKrishna.8921@gmail.com>
2024-09-26 19:31:13 +0530

    added configuration in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: { sans: ["Fira Code", "monospace"] },
     extend: {
       colors: {
         "theme-main": "#011627",
@@ -12,9 +14,45 @@ export default {
         "accent-variable": "#43D9AD",
         "accent-underline": "#FEA55F",
 
+        // border
+        "border-white": "#1E2D3D",
+
         // text color
         "white-gradient-01": "#E5E9F0",
         "gray-gradient-01": "#607B96",
+        "code-keyword": "#C98BDF",
+        "code-rest": "#5565E8",
+        "code-id": "#FEA55F",
+
+        // form fields
+        "bg-field-default": "#011221",
+
+        // button color
+        "bg-button-default": "#1C2B3A",
+      },
+
+      borderRadius: {
+        sm: "8px",
+      },
+
+      fontSize: {
+        "3xl": "2rem",
+        "6xl": "3.875rem",
+      },
+
+      margin: {
+        82: "5.125rem",
+      },
+
+      gap: {
+        92: "5.75rem",
+      },
+
+      inset: {
+        7: "7rem",
+        9: "9rem",
+        11: "11rem",
+        13: "13rem",
       },
     },
   },
